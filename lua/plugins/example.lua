d14751ce78930e1051e5745503100246daf64314
StaZarycki <stazarycki@gmail.com>
2025-03-24 11:04:40 +0100

    kanagawa color scheme

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -12,6 +12,9 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
+  -- add kanagawa
+  { "rebelot/kanagawa.nvim" },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
