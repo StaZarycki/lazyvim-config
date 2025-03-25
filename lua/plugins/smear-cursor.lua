4d485a054c9b14ce126e6fd3ab6a121fc2d9d3b4
StaZarycki <stazarycki@gmail.com>
2025-03-25 17:08:38 +0100

    smear cursor config

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,23 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  event = "VeryLazy",
+  cond = vim.g.neovide == nil,
+  opts = {
+    hide_target_hack = true,
+    cursor_color = "none",
+    smear_between_buffers = false,
+    stiffness = 0.5,
+    trailing_stiffness = 0.49,
+    never_draw_over_target = false,
+  },
+  specs = {
+    -- disable mini.animate cursor
+    {
+      "echasnovski/mini.animate",
+      optional = true,
+      opts = {
+        cursor = { enable = false },
+      },
+    },
+  },
+}
